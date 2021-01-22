6e5b9075b6703e2ae26de81767a40f67401b0c61
Guilherme Ytalo <guilherme.azevedo@bitzen.com.br>
2021-01-22 09:14:41 -0300

    New Screen

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import StarterPage from './src/screens/starter-page/StarterPage';
 import Login from './src/screens/login/Login';
 import Register from './src/screens/register/Register';
 import Home from './src/screens/home/Home';
 
-import { ThemeProvider } from 'styled-components';
-import { SafeAreaView } from './AppStyle';
+import {ThemeProvider} from 'styled-components';
+import {SafeAreaView} from './AppStyle';
 
 const Stack = createStackNavigator();
 
 const baseColors = {
   primary: '#1B1724',
-  secondary: '#ffffff'
-}
+  secondary: '#ffffff',
+};
 
 const App = () => {
   return (
     <SafeAreaView>
-    <ThemeProvider theme={baseColors}>
+      <ThemeProvider theme={baseColors}>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="StarterPage">
             <Stack.Screen name="StarterPage" component={StarterPage} />
@@ -30,7 +30,7 @@ const App = () => {
             <Stack.Screen name="Home" component={Home} />
           </Stack.Navigator>
         </NavigationContainer>
-      </ThemeProvider>      
+      </ThemeProvider>
     </SafeAreaView>
   );
 };
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
 
-import { 
-    Container, 
-    Body,
-    Title, 
-    SubTitle,    
-    ButtonView,
-    SignIn    
+import {
+  Body,
+  ButtonView,
+  Container,
+  SignIn,
+  SubTitle,
+  Title,
 } from './LoginStyle';
 
 export default function Login() {
-    return(
-        <Container>
-            <Body>
-                <Title>Let's sign you in.</Title>
-                <SubTitle>Welcome back.</SubTitle>                
-                <SubTitle>You've been missed!</SubTitle>
-            
-                
-            </Body>
+  return (
+    <Container>
+      <Body>
+        <Title>Let's sign you in.</Title>
+        <SubTitle>Welcome back.</SubTitle>
+        <SubTitle>You've been missed!</SubTitle>
+      </Body>
 
-            <ButtonView>
-                <SignIn title='SignIn'></SignIn>                
-            </ButtonView>
-        </Container>
-    );
+      <ButtonView>
+        <SignIn title="SignIn" />
+      </ButtonView>
+    </Container>
+  );
 }
